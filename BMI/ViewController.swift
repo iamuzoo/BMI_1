8c85c7d210204fb9d9d94d86ebb8fd4bdd2dda0e
will <will@specialistapps.com>
2018-11-08 15:11:02 +1100

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Will on 8/11/18.
+//  Copyright © 2018 Will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
